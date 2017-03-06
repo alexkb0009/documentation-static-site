027115b994e425db58b4f5d565ecd6e79b21373c
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-03-06 11:56:30 -0500

    TODO statements

--- __main__.py
+++ __main__.py
@@ -1,18 +1,39 @@
+import argparse
 import subprocess
+import yaml
 
 # Install dependencies
 def install_dependencies():
     print("Updating dependencies")
     result = subprocess.run(["pip3", "install", "-r", "./requirements.txt"])
     if (result.returncode == 0):
-        print('...success!')
+        print('Successfully installed dependencies!')
         return True
     else:
         result.check_returncode()
         return False
 
+def load_configuration():
+    pass
+    # TODO: Load YAML
+    #yaml.load()
+
+
+configuration = {}
+
+
 print("Running document static site generator...")
 install_successful = install_dependencies()
 
+parser = argparse.ArgumentParser(description="Process options.")
+args = parser.parse_args()
+pass
+
 if not install_successful:
     print("Shutting down")
+
+# TODO: Figure out what's needed for RST files and /documentation/ dir.
+# TODO: Generate /documentation/ dir full of required RST files.
+#   TODO: Sphinx autodoc for PyDoc -> RST files
+#   TODO: JSDoc-> RST Files
+# TODO : Sphinx-build -> HTML
